4f305dadc267666ec93ed1a6ea0269840984100b
goodingc5066 <47424280+goodingc5066@users.noreply.github.com>
2020-01-15 09:49:09 -0500

    Add files via upload
    Demo DAY2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carDoors = 4;
+            Console.WriteLine(carDoors);
+            Console.ReadLine();
+        
+        }
+    }
+}
